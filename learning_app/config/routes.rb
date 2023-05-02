2eed3ae8aa6ee3fa1cc4b957959d4fb95e5acb57
Satyam <satyam.tripathi@w3villa.com>
2023-05-02 15:11:57 +0530

    intial state checking

--- learning_app/config/routes.rb
+++ learning_app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "homes#index"
-  get "/admin"
+  # get "/admin"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
